f6f59fb6c327959b1854f93db3b54df7e1d5a3e0
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-02-24 21:57:32 +0500

    commit

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
-console.log(Boolean(1));
-console.log(Boolean(0));
-console.log(Boolean('hello'));
-console.log(Boolean(''));
-console.log(Boolean());
-console.log(Boolean(' '));
+// Task-1
+// String Convertation
+
+let valueString = false; 
+console.log(typeof valueString); // Boolean
+
+valueString = String(valueString);
+console.log(typeof valueString); // String
+
+// Number Convertation
+
+let valueNumber = "19";
+console.log(typeof valueNumber); // String
+
+valueNumber = String(valueNumber);
+console.log(typeof valueNumber); // Number
+
+// Boolean Convertation
+
+let valueBoolean_1 = 1; // Number
+console.log(typeof Boolean(valueBoolean_1)); // true
+let valueBoolean_2 = 0; // Number
+console.log(typeof Boolean(valueBoolean_2)); // false
+
+let valueBoolean_3 = "Jumanazar"; // String
+console.log(typeof Boolean(valueBoolean_3)); // true
+let valueBoolean_4 = ""; // Null
+console.log(typeof Boolean(valueBoolean_4)); // false
+
+let valueBoolean_5 = "0"; // String
+console.log(typeof Boolean(valueBoolean_5)); // true
+let valueBoolean_6 = " "; // Null
+console.log(typeof Boolean(valueBoolean_6)); // true
+
+
+
+// Task-2
+
+
+
+
+
+
+
+
+
